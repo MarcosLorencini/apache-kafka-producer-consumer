2be9267b8c5135c03f8c147e0c1d0c50827cf735
MarcosLorencini <mlorencinif@gmail.com>
2023-05-23 16:33:01 -0300

    first commit

---
+++ str-producer/src/main/java/com/kafka/strproducer/anotacoes.java
@@ -0,0 +1,9 @@
+package com.kafka.strproducer;
+
+public class anotacoes {
+
+    //aula 6 - subir as instâncias do zookeeaper, do Apache Kafka e do kafkadrop utilizando o docker compose
+    //o arquivo docker-compose.yml possui as informações para subir as 3 intancias:
+    // zookeeper precisa subir primeiro para rodar o apache kafika, kafika e o kfdrop
+    //
+}
