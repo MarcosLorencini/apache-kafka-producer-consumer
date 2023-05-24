d0f84289b9a86322fbe6663d06c0b258ec6e1e1c
MarcosLorencini <mlorencinif@gmail.com>
2023-05-24 17:47:54 -0300

    Sending the first msg to topic

--- str-producer/src/main/java/com/kafka/strproducer/services/SpringProducerService.java
+++ str-producer/src/main/java/com/kafka/strproducer/services/SpringProducerService.java
@@ -1,4 +1,22 @@
 package com.kafka.strproducer.services;
 
+//Service que vai enviar msg para o topico
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SpringProducerService {
+
+    //Já definido como bean na config
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("str-topic", message);
+    }
+
+
 }
