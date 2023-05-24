921d95bd1a12ed9304afe7ca3c4a59ec7d54a832
MarcosLorencini <mlorencinif@gmail.com>
2023-05-24 17:05:18 -0300

    kafkaAdmin configurated

--- str-producer/src/main/java/com/kafka/strproducer/anotacoes.java
+++ str-producer/src/main/java/com/kafka/strproducer/anotacoes.java
@@ -4,6 +4,14 @@ public class anotacoes {
 
     //aula 6 - subir as instâncias do zookeeaper, do Apache Kafka e do kafkadrop utilizando o docker compose
     //o arquivo docker-compose.yml possui as informações para subir as 3 intancias:
-    // zookeeper precisa subir primeiro para rodar o apache kafika, kafika e o kfdrop
-    //
+    // zookeeper precisa subir primeiro para rodar o apache kafka, kafka e o kfdrop
+    // o docker-compose up vai subir o container: vai fazer pull das imagens e subir os 3 serviços
+    //subindo ir no navegador e acessar o kfdrop(overview do cluster kafka e pode criar topicos)
+    // localhost:19000
+    //ctr + c estopa o docker, para os 3 serviços
+    //docker container ls: verifica quais containers estao rodando
+    //docker container ls -a: mostra os que estão rodando e os que já rodaram
+    //docker images: são imagens que já foram baixadas
+    //docker container up -d : sob as imagens sem deixar o terminal travado(destachar o terminal)
+    //docker-compose down : stopa os serviços
 }
--- str-producer/src/main/java/com/kafka/strproducer/config/KafkaAdminConfig.java
+++ str-producer/src/main/java/com/kafka/strproducer/config/KafkaAdminConfig.java
@@ -1,7 +1,37 @@
 package com.kafka.strproducer.config;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaAdminConfig {
+
+    //coloquei ele como final e usei a anotação @RequiredArgsConstructor para criar um construtor
+    public final KafkaProperties properties;
+
+    //criar um kakfa admin para fazer a comunicação com o kafka cluster(kfdrop)
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+
+    //toda a vez que iniciar o projeto criar o topico
+    @Bean
+    public KafkaAdmin.NewTopics topics() {
+        return new KafkaAdmin.NewTopics(
+                TopicBuilder.name("str-topic").partitions(2).replicas(1).build()
+        );
+    }
+
+
 }
