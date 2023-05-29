9ca08041aa27505b910917d0d0c29bb370fb98b0
MarcosLorencini <mlorencinif@gmail.com>
2023-05-29 14:53:42 -0300

    add calback function to send message function

--- str-producer/src/main/java/com/kafka/strproducer/services/SpringProducerService.java
+++ str-producer/src/main/java/com/kafka/strproducer/services/SpringProducerService.java
@@ -3,11 +3,14 @@ package com.kafka.strproducer.services;
 //Service que vai enviar msg para o topico
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class SpringProducerService {
 
@@ -15,7 +18,16 @@ public class SpringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message) {
-        kafkaTemplate.send("str-topic", message);
+        kafkaTemplate.send("str-topic", message).addCallback(
+                success -> {
+                    if(success != null) {
+                        log.info("Partition {}, Offset {}",
+                                success.getRecordMetadata().partition(),
+                                success.getRecordMetadata().offset());
+                    }
+                },
+                error -> log.error("Error send message")
+        );
     }
 
 
