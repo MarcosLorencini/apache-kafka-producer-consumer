df093bb152054bcf233158c0f28582fe380a3602
MarcosLorencini <mlorencinif@gmail.com>
2023-05-29 16:09:30 -0300

    partition defined

--- str-consumer/src/main/java/com/kafika/strconsumer/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/kafika/strconsumer/strconsumer/listeners/StrConsumerListener.java
@@ -2,6 +2,7 @@ package com.kafika.strconsumer.strconsumer.listeners;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,15 +12,27 @@ public class StrConsumerListener {
     //defini o nome do groupId que este consumer pertence, no caso ele percente ao groupId = group1
     //escuta do topico de nome str-topic
     //utiliza o strContainerFactory(definido no arquivo de config) para fazer a leitura do topico
-    @KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+
+    //definido uma partição "0" para este metodo do mesmo grupo
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                    @TopicPartition(topic = "str-topic", partitions = {"0"}) //array de partições
+            },
+            containerFactory = "strContainerFactory")
     public void create(String message) {
         log.info("CREATE ::: Receive message {}", message);
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    //definido uma partição "1" para este metodo do mesmo grupo
+    @KafkaListener(groupId = "group-1",
+            topicPartitions = {
+                @TopicPartition(topic = "str-topic", partitions = {"1"}) //array de partições
+            },
+            containerFactory = "strContainerFactory")
     public void log(String message) {
         log.info("LOG ::: Receive message {}", message);
     }
+
 
     @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
     public void history(String message) {
