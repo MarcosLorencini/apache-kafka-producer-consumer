839563486d5906d1a119be851385a4e6b7c1fefb
MarcosLorencini <mlorencinif@gmail.com>
2023-05-30 11:17:30 -0300

    custom listener

---
+++ str-consumer/src/main/java/com/kafika/strconsumer/strconsumer/custom/StrConsumerCustomListener.java
@@ -0,0 +1,34 @@
+package com.kafika.strconsumer.strconsumer.custom;
+
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//nesta classe estamos criando um listener customizado, para não replicar código na
+// classe SrtConsumerListener.java
+
+//informa ao compilador java e para JVM que esta anotação deve ester disponível por meio de reflexão
+//tem tempo de exercução
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@KafkaListener
+public @interface StrConsumerCustomListener {
+
+    //pode ser usado para decorar atributos dentro de uma anotação
+    //atributo topics customizado para o nome padrão "str-topic"
+    @AliasFor(annotation = KafkaListener.class, attribute = "topics")
+    String[] topics() default "str-topic";
+
+    @AliasFor(annotation = KafkaListener.class, attribute = "containerFactory")
+    String containerFactory() default "strContainerFactory";
+
+    //como existe grupos diferentes, não tem com o mencionar um groupId default
+    // temos que definir na clase StrConsumerListener
+    @AliasFor(annotation = KafkaListener.class, attribute = "groupId")
+    String groupId() default "";
+}
--- str-consumer/src/main/java/com/kafika/strconsumer/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/kafika/strconsumer/strconsumer/listeners/StrConsumerListener.java
@@ -1,5 +1,6 @@
 package com.kafika.strconsumer.strconsumer.listeners;
 
+import com.kafika.strconsumer.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.TopicPartition;
@@ -9,10 +10,31 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class StrConsumerListener {
 
-    //defini o nome do groupId que este consumer pertence, no caso ele percente ao groupId = group1
+    //definir o nome do groupId que este consumer pertence, no caso ele percente ao groupId = group1
     //escuta do topico de nome str-topic
     //utiliza o strContainerFactory(definido no arquivo de config) para fazer a leitura do topico
 
+    //Observacao: no minimo a quantidade de consumer deve ser equivalente a quantidade de partiçoes
+    //caso eles estejam cadastrado no mesmo groupId
+
+    //Pode ter groupId diferentes olhando para o mesmo topico(em paralelo)
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message);
+    }
+
+    /*
     //definido uma partição "0" para este metodo do mesmo grupo
     @KafkaListener(groupId = "group-1",
             topicPartitions = {
@@ -37,10 +59,7 @@ public class StrConsumerListener {
     @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
     public void history(String message) {
         log.info("HISTORY ::: Receive message {}", message);
-    }
+    }*/
 
-    //Observacao: no minimo a quantidade de consumer deve ser equivalente a quantidade de partiçoes
-    //caso eles estejam cadastrado no mesmo groupId
 
-    //Pode ter groupId diferentes olhando para o mesmo topico(em paralelo)
 }
